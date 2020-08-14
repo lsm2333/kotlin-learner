f00ef9c6947105cbda2a9f38f6ee16755c8c619a
shengminglin <shengming.lin@xiaohongshu.com>
2020-08-14 14:16:01 +0800

    add when statement chapter

---
+++ src/main/kotlin/controlFlow/WhenStatement.kt
@@ -0,0 +1,23 @@
+package controlFlow
+
+fun cases(obj: Any) {
+    //所有条件被按顺序匹配，第一个满足的分支会被执行
+    when (obj) {
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("Long")
+        !is String -> println("Not a string")
+        else -> println("Unknown")// else表达默认情况，可以省略
+    }
+}
+
+
+fun main() {
+    cases("Hello")
+    cases(1)
+    cases(1L)
+    cases(MyClass())
+    cases("hello")
+}
+
+class MyClass
