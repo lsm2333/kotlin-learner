87a19c848d8a2677a8a9136da18ce7941d854321
shengminglin <shengming.lin@xiaohongshu.com>
2020-08-17 19:44:51 +0800

    add chapter for storing properties in map

---
+++ src/main/kotlin/delegation/delegationPattern/StoringInMap.kt
@@ -0,0 +1,27 @@
+package delegation.delegationPattern
+
+class User(map: Map<String, Any?>) {
+    //根据属性名从map中找到key获取value
+    val name: String by map
+    val age: Int     by map
+}
+
+/**
+ * 可以利用map来存属性，很适合处理json
+ *
+ * @date 2020-08-17 19:42
+ * @param
+ * @return
+ * @author shengming.lin
+ * @see
+ **/
+fun main() {
+    val user = User(
+        mapOf(
+            "name" to "John Doe",
+            "age" to 25
+        )
+    )
+
+    println("name = ${user.name}, age = ${user.age}")
+}
