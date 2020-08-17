61638b6ce1b6d1a5c135ff87b5d3322a202d041f
shengminglin <shengming.lin@xiaohongshu.com>
2020-08-17 13:53:55 +0800

    add chapter for all

---
+++ src/main/kotlin/collections/function/anyAllNone/All.kt
@@ -0,0 +1,14 @@
+package collections.function.anyAllNone
+
+fun main() {
+
+    val numbers = listOf(1, -2, 3, -4, 5, -6)            // 1
+
+    //都满足时返回true
+    val allEven = numbers.all { it % 2 == 0 }            // 2
+
+    val allLess6 = numbers.all { it < 6 }                // 3
+
+    println(allEven)
+    println(allLess6)
+}
